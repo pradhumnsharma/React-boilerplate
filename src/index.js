c3e10a579b66726fc0789550fd6b6a19d458df4d
Pradhumn Sharma <chris.m.labiak@gmail.com>
2021-04-28 20:28:10 +0530

    Added SCSS and images configuration

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import './styles/index.scss';
 
-const App = () => <h1>QWERR</h1>;
+const App = () => (
+  <h1 className="main-heading">
+    This is first page
+  </h1>
+);
 
 ReactDOM.render(<App />, document.getElementById('root'));
